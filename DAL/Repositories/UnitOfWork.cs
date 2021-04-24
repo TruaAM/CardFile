d0745b5072afbab92f9db6edbd9dd01d4a3b0eac
TruaAM <trunovalexander8@gmail.com>
2021-04-24 20:22:11 +0300

    Interfaces and UnitOfWork

---
+++ DAL/Repositories/UnitOfWork.cs
@@ -0,0 +1,41 @@
+﻿using DAL.Data;
+using DAL.Interfaces;
+using System;
+
+namespace DAL.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private DBContext _db;
+
+        public UnitOfWork()
+        {
+            _db = new DBContext();
+        }
+
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+
+        private bool disposed = false;
+
+        public virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    _db.Dispose();
+                }
+                this.disposed = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
