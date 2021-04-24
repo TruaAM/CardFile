ae461bf490472658fd7e8306b81e6b756cacfebc
TruaAM <trunovalexander8@gmail.com>
2021-04-24 19:52:05 +0300

    Fixes and DbContext

---
+++ DAL/Data/DbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace DAL.Data
+{
+    public class DBContext : DbContext
+    {
+        private readonly string _conStr;
+        public DBContext()
+        {
+            var builder = new ConfigurationBuilder();
+            string fileName = @"pathdatabase\DatabaseConnection.json";
+            builder.AddJsonFile(Path.GetFullPath(fileName));
+            IConfigurationRoot config = builder.Build();
+            _conStr = config["ConnectionString"];
+            Database.EnsureCreated();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_conStr);
+        }
+    }
+}
